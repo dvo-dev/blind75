3385f44a67d90de4062f48b9cbdaacf639321b6e
dvo-dev <dannyphvo@gmail.com>
2022-05-11 16:22:48 -0700

    Time: 0 ms (100.00%), Space: 2 MB (30.61%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.go
+++ 213-house-robber-ii/213-house-robber-ii.go
@@ -18,17 +18,12 @@ func rob(nums []int) int {
         return sum1
     }
     
-    // Edge case
-    if len(nums) == 1 {
-        return nums[0]
-    }
-    
     // There are two house robber 1 subproblems
     // If you choose to rob the first house you cannot rob the last house
     // and vice-versa
-    return max(
+    return max(nums[0], max(    // Edge case of one house
         _rob(nums[:len(nums)-1]),
         _rob(nums[1:]),
-    )
+    ))
 }
 
