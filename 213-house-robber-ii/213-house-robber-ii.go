45ad5c80028b6182a251962d3d23937f0be4c753
dvo-dev <dannyphvo@gmail.com>
2022-05-11 16:15:54 -0700

    Time: 1 ms (45.77%), Space: 1.9 MB (100.00%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.go
+++ 213-house-robber-ii/213-house-robber-ii.go
@@ -6,6 +6,8 @@ func rob(nums []int) int {
         
         return b
     }
+    
+    // House robber 1 solution can be reused
     _rob := func(homes []int) int {
         sum0, sum1 := 0, 0
         for _, n := range homes {
@@ -16,10 +18,14 @@ func rob(nums []int) int {
         return sum1
     }
     
+    // Edge case
     if len(nums) == 1 {
         return nums[0]
     }
     
+    // There are two house robber 1 subproblems
+    // If you choose to rob the first house you cannot rob the last house
+    // and vice-versa
     return max(
         _rob(nums[:len(nums)-1]),
         _rob(nums[1:]),
