33995fc7b962955d299a32a2f08c8012385d1976
dvo-dev <dannyphvo@gmail.com>
2022-05-23 19:58:17 -0700

    Time: 23 ms (17.74%), Space: 6 MB (22.87%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.go
@@ -0,0 +1,39 @@
+func topKFrequent(nums []int, k int) []int {
+    // First determine the frequency of each unique value in the array
+    frequency := make(map[int]int)
+    for _, n := range nums {
+        val, ok := frequency[n]
+        if ok {
+            frequency[n] = val + 1
+        } else {
+            frequency[n] = 1
+        }
+    }
+    
+    // Rearrange it so we have a slice with index representing frequency,
+    // values being a slice with values that occur at the respective freq
+    count := make([][]int, len(nums)+1)
+    for val, freq := range frequency {
+        count[freq] = append(count[freq], val)
+    }
+    
+    res := make([]int, k)
+    for i := len(count) - 1; i >= 0; i-- {
+        if k-1 < 0 {
+            break
+        }
+        
+        if len(count[i]) > 0 {
+            for _, n := range count[i] {
+                if k-1 < 0 {
+                    return res
+                }
+                
+                res[k-1] = n
+                k -= 1
+            }
+        }
+    }
+    
+    return res
+}
